ac08fec106de35897ed79a163bec557922e20675
liuwei <liuwei@liuwei-MacBook-Pro.local>
2020-04-29 14:41:31 +0800

    imported *rc.js

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+// prettier config
+// https://prettier.io/docs/en/options.html
+
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+};
